2aaf3967df937acaaf99e7f15ea5eb3d6f5cffb7
yonaikerlol <39972049+yonaikerlol@users.noreply.github.com>
2018-10-09 00:04:21 -0400

    Update extractor-pelisplus.co.sh

--- extractor-pelisplus.co.sh
+++ extractor-pelisplus.co.sh
@@ -45,7 +45,7 @@ echo "
 
 "
 if [ -z "${1}" ] || [ -z "${2}" ]; then
-	echo -e "Usage: ${0} {serie} {episodes of 1 season} {episodes of 2 season}...{episodes of 15 season}\nExample: ${0} mr-robot 10 12 10"
+	echo -e "Usage: ${0} <id of serie> <episodes of 1 season> <episodes of 2 season>...<episodes of 15 season>\nExample: ${0} mr-robot 10 12 10"
 	exit 0
 fi
 
